78e8131ec58204350dba8c5e20f1660546343a04
pedros.gomez <pedros.gomez@softtek.com>
2019-10-23 22:55:46 -0500

    The HelloWorld.java file was added

---
+++ HelloWorld.java
@@ -0,0 +1,23 @@
+package helloworld;
+
+/**
+ *
+ * @author Pedro Sergio Gomez S
+ */
+public class HelloWorld {
+
+    public void Greeting(){
+        
+        System.out.println("Hello World");
+        
+    }
+    
+    public static void main(String[] args) {
+    
+        HelloWorld o1 = new HelloWorld();
+        
+        o1.Greeting();
+        
+    }
+    
+}
